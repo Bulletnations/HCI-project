8f6cccdbf4a9856cfd57d39bf2457d0ec31b8179
Bulletnation <osewebrian5@gmail.com>
2023-02-04 19:48:07 +0300

    home layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class ="container">
+       <div class = "logo">Logo</div>
+       <div class = "header">Header</div>
+       <div class = "nav">nav</div>
+       <div class = "content"> 
+            <div class = "profile">profile1</div>
+            <div class = "profile">profile2</div>
+            <div class = "profile">content</div>
+            <div class = "profile">content</div>
+       </div>
+       <div class = "footer">footer</div>
+       
+
     </div>
   );
 }
